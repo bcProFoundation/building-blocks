fe9b9564caa3143016561dc03ee3ce651d67f52d
Gaurav Naik <gvrvnk@gmail.com>
2018-07-14 16:06:33 +0530

    refactor(identity-provider) Remove unused imports
    Remove unused imports from app.module.ts

--- apps/identity-provider/src/server/app.module.ts
+++ apps/identity-provider/src/server/app.module.ts
@@ -4,19 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ModelsModule } from './models/models.module';
-import { ExpressSessionMiddleware } from '@nest-middlewares/express-session';
-import { CookieParserMiddleware } from '@nest-middlewares/cookie-parser';
-import {
-  PassportInitializeMiddleware,
-  PassportSessionMiddleware,
-} from '@nest-middlewares/passport';
-import { TypeormStore } from 'nestjs-session-store';
-import { SessionService } from 'models/session/session.service';
-import { UserService } from 'models/user/user.service';
 import { ConfigModule } from 'config/config.module';
 import { ConfigService } from 'config/config.service';
 import { ServerSideRenderingMiddleware } from 'auth/middlewares/server-side-rendering.middleware';
-import { OAuth2Controller } from 'auth/controllers/oauth2/oauth2.controller';
 
 const config = new ConfigService();
 
