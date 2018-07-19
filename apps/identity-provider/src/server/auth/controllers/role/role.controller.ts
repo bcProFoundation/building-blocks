83a970ea9c0f40fde07e96d1b0b6369d5cad30eb
Gaurav Naik <gvrvnk@gmail.com>
2018-07-19 20:15:32 +0530

    refactor(identity-provider) RoleGuard Updates
    Ensure consistency for local and bearer strategy while using RoleGuard

--- apps/identity-provider/src/server/auth/controllers/role/role.controller.ts
+++ apps/identity-provider/src/server/auth/controllers/role/role.controller.ts
@@ -6,21 +6,32 @@ import {
   Get,
   Delete,
   Patch,
+  Res,
 } from '@nestjs/common';
-import { callback } from '../../passport/local.strategy';
+import { callback } from '../../passport/http-bearer.strategy';
 import { Roles } from '../../guards/roles.decorator';
 import { AuthGuard } from '../../guards/auth.guard';
 import { RolesGuard } from '../../guards/roles.guard';
+import { EnsureLoginGuard } from 'nestjs-ensureloggedin-guard';
 
 @Controller('role')
-@UseGuards(AuthGuard('bearer', { session: false, callback }), RolesGuard)
 export class RoleController {
   @Post('create')
   create(@Body() body) {}
 
-  @Get('read')
+  @Get('read-bearer')
   @Roles('admin')
-  read(@Body() payload) {}
+  @UseGuards(AuthGuard('bearer', { session: false, callback }), RolesGuard)
+  readBearer(@Body() payload, @Res() res) {
+    res.json({ bearer: 'Yas!' });
+  }
+
+  @Get('read-session')
+  @Roles('admin')
+  @UseGuards(EnsureLoginGuard, RolesGuard)
+  readSession(@Body() payload, @Res() res) {
+    res.json({ session: 'Yas!' });
+  }
 
   @Patch('update')
   update(@Body() payload) {}
