74c855758559ff574f0717eb9291c312f5657495
Revant Nandgaonkar <revant.one@gmail.com>
2018-07-26 21:51:13 +0530

    refactor(identity-provider) remove cli library
    Remove console connector library

--- apps/identity-provider/src/server/cli-connector/create-client.ts
+++ apps/identity-provider/src/server/cli-connector/create-client.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
 import { ClientService } from '../models/client/client.service';
-import { createIDPClientCLI } from 'nestjs-console-connector';
 import { Client } from '../models/client/client.entity';
+import * as yargs from 'yargs';
 
 const app = NestFactory.create(AppModule);
 const args = createIDPClientCLI();
@@ -15,3 +15,24 @@ app.then(async r => {
   client.isTrusted = 1;
   clientService.save(client).then(() => process.exit());
 });
+
+function createIDPClientCLI() {
+  return yargs
+    .option('client', {
+      alias: 'c',
+      describe: 'client name',
+    })
+    .option('redirect_uri', {
+      alias: 'r',
+      describe: 'redirect_uri for client',
+    })
+    .option('secret', {
+      alias: 's',
+      describe: 'client_secret for client',
+    })
+    .demandOption(
+      ['client', 'redirect_uri', 'secret'],
+      'Please provide client name, redirect_uri and secret arguments.',
+    )
+    .help().argv;
+}
