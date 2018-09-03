cae5135029a3f3485a61e257060425be385d2163
Revant Nandgaonkar <revant.one@gmail.com>
2018-09-03 21:56:31 +0530

    update(authorization-server) Update NestJs
    update nestjs and libraries to latest
    implemented onApplicationBootstrap in oauth2orize.setup

--- apps/authorization-server/src/client/app/login/login-data.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class LoginDataService {}
