a614f275166d5c12d4c6b3fc6b1d77c52e126d0d
Gaurav <gvrvnk@gmail.com>
2018-10-14 11:51:27 +0530

    fix(infrastructure console): Fix createClient, menu icon
    Add messaging to createClient method of client.component.ts
    Fix menu icon in dashboard.nav

--- apps/infrastructure-console/src/client/app/constants/messages.ts
+++ apps/infrastructure-console/src/client/app/constants/messages.ts
@@ -2,3 +2,4 @@ export const CLIENT_VERIFIED = 'Client verified.';
 export const CLIENT_UNVERIFIED = 'Client unverified.';
 export const CLIENT_CREATED = 'Client created Successfully.';
 export const CLIENT_UPDATED = 'Client updated Successfully.';
+export const CLIENT_ERROR = 'Could not create client.';
