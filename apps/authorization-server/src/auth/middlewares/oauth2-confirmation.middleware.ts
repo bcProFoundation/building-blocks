90c69a97950ca3ec459ebd42d388de039181d789
Revant Nandgaonkar <revant.one@gmail.com>
2019-04-17 22:57:58 +0530

    fix(authorization-server): Revert to unhandled exception
    Middleware exception is not handled
    https://github.com/nestjs/docs.nestjs.com/issues/215
    https://github.com/nestjs/nest/issues/294
    
    revert error handling

--- apps/authorization-server/src/auth/middlewares/oauth2-confirmation.middleware.ts
+++ apps/authorization-server/src/auth/middlewares/oauth2-confirmation.middleware.ts
@@ -5,6 +5,8 @@ import { OAuth2orizeSetup } from './oauth2orize.setup';
 export class OAuth2ConfirmationMiddleware implements NestMiddleware {
   constructor(private readonly oauthorizeSetup: OAuth2orizeSetup) {}
   public use(request, response, next: () => void) {
+    // TODO: https://github.com/nestjs/docs.nestjs.com/issues/215
+    // Middleware failure results in internal server error
     this.oauthorizeSetup.getCodeGrantMiddleware()(request, response, next);
   }
 }
