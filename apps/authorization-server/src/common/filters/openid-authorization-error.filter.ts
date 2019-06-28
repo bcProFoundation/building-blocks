c7dc9a9aa24714cdee272b7c3a8131abd38532d2
Revant Nandgaonkar <revant.one@gmail.com>
2019-06-29 05:13:01 +0530

    fix(authorization-server): Filter OAuth 2 OpenID Errors
    Add filter to show meaningful message
    
    show meaningful error messages for errors thrown during oidc

---
+++ apps/authorization-server/src/common/filters/openid-authorization-error.filter.ts
@@ -0,0 +1,10 @@
+import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
+import * as AuthorizationError from 'oauth2orize-openid/lib/errors/authorizationerror';
+
+@Catch(AuthorizationError)
+export class OpenIDAuthorizationErrorFilter implements ExceptionFilter {
+  catch(error: AuthorizationError, host: ArgumentsHost) {
+    const response = host.switchToHttp().getResponse();
+    response.status(error.status).json({ error: error.message });
+  }
+}
