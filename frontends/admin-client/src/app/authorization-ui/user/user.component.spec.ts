25bbfbef0f1d6d5c13b56b71130c623aad136989
Revant Nandgaonkar <revant.one@gmail.com>
2020-11-15 15:03:46 +0530

    update: root and packages
    update root dependencies
    update packages dependencies
    
    use latest dependencies

--- frontends/admin-client/src/app/authorization-ui/user/user.component.spec.ts
+++ frontends/admin-client/src/app/authorization-ui/user/user.component.spec.ts
@@ -1,41 +1,52 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UserComponent } from './user.component';
 import { MaterialModule } from '../../shared-imports/material/material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpErrorHandler } from '../../common/services/http-error-handler/http-error-handler.service';
 import { MessageService } from '../../common/services/message/message.service';
 import { oauthServiceStub } from '../../common/testing-helpers';
-import { OAuthService } from 'angular-oauth2-oidc';
-import { RouterTestingModule } from '@angular/router/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@Component({
+  selector: 'claims-listing',
+  template: '',
+})
+export class ClaimsListingComponent {
+  @Input('uuid') uuid: string;
+}
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        MaterialModule,
-        FormsModule,
-        ReactiveFormsModule,
-        HttpClientTestingModule,
-        RouterTestingModule,
-        BrowserAnimationsModule,
-      ],
-      providers: [
-        HttpErrorHandler,
-        MessageService,
-        {
-          provide: OAuthService,
-          useValue: oauthServiceStub,
-        },
-      ],
-      declarations: [UserComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          MaterialModule,
+          FormsModule,
+          ReactiveFormsModule,
+          HttpClientTestingModule,
+          RouterTestingModule,
+          BrowserAnimationsModule,
+        ],
+        providers: [
+          HttpErrorHandler,
+          MessageService,
+          {
+            provide: OAuthService,
+            useValue: oauthServiceStub,
+          },
+        ],
+        declarations: [UserComponent, ClaimsListingComponent],
+      }).compileComponents();
+    }),
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
